3a88cac188a65435948bd4c9e68713e2f0e42eb7
andreleao1 <andre.gustavo.ls@hotmail.com>
2024-02-24 13:10:40 -0300

    first commit

---
+++ src/main/java/br/com/agls/expensesauthapi/ExpensesAuthApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.agls.expensesauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesAuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/agls/expensesauthapi/ExpensesAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agls.expensesauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
